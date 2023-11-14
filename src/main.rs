2a6aeff78bd107072e95566358fdf24a90077c6a
Ivan Gonzalez <scratchmex@gmail.com>
2023-11-14 17:57:48 -0600

    working with sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,197 @@
 use anyhow::Result;
 use itertools::Itertools;
+use rusqlite::{self, params, params_from_iter};
 use std::fs;
 
-fn create_table(fragment: &String) {
-    // skip header
-    let mut it = fragment
-        .strip_prefix("create table")
-        .expect(&format!("not a create table?: {}", fragment))
-        .chars()
-        .skip_while(|c| c.is_whitespace())
-        .peekable();
+struct ImportOp {
+    content: String,
+    connection: rusqlite::Connection,
+}
 
-    // table name
-    let table_name: String = it
-        .by_ref()
-        .take_while(|c| !c.is_whitespace())
-        // strip ticks or []
-        .filter(|c| !['\'', '"', '[', ']'].contains(&c))
-        .collect();
+impl ImportOp {
+    pub fn new(filepath: &str) -> Self {
+        let content = fs::read_to_string(filepath).unwrap();
+        let connection = rusqlite::Connection::open("db.db").unwrap();
 
-    println!("table name: {:?}", table_name);
+        ImportOp {
+            content,
+            connection,
+        }
+    }
 
-    it.by_ref()
-        .take_while(|&c| c.is_whitespace())
-        .take_while(|&c| c == '(')
-        .for_each(drop);
+    pub fn parse(&self) -> Result<()> {
+        let mut lines = self.content.split("\n").peekable();
 
-    // col names
-    let mut col_names = Vec::<String>::new();
-    while let Some(&cpeak) = it.peek() {
-        // skip spaces
-        if cpeak.is_whitespace() {
-            it.next();
-            continue;
+        while let Some(&lpeak) = lines.by_ref().peek() {
+            if lpeak.starts_with("--") || lpeak.is_empty() {
+                lines.next();
+                continue;
+            }
+
+            let fragment = lines
+                .by_ref()
+                .take_while_inclusive(|l| !l.ends_with(";"))
+                .collect::<String>()
+                .to_lowercase();
+
+            if fragment.starts_with("create table") {
+                self.create_table(&fragment)?;
+            } else if fragment.starts_with("copy") {
+                self.copy_to_table(&fragment, lines.by_ref())?;
+                // break;
+            } else {
+                // println!("skipping {:?}", fragment);
+                continue;
+            }
         }
 
-        // take until a new space
-        let col_name: String = it
+        Ok(())
+    }
+
+    fn create_table(&self, fragment: &String) -> Result<()> {
+        // skip header
+        let mut it = fragment
+            .strip_prefix("create table")
+            .expect(&format!("not a create table?: {}", fragment))
+            .chars()
+            .skip_while(|c| c.is_whitespace())
+            .peekable();
+
+        // table name
+        let table_name: String = it
             .by_ref()
             .take_while(|c| !c.is_whitespace())
             // strip ticks or []
             .filter(|c| !['\'', '"', '[', ']'].contains(&c))
             .collect();
 
-        // skip until comma or end of statement
-        let _type: String = it.by_ref().take_while(|&c| c != ',').collect();
+        println!("table name: {:?}", table_name);
 
-        // println!("{:?} has type {:?}", col_name, _type);
+        it.by_ref()
+            .take_while(|&c| c.is_whitespace())
+            .take_while(|&c| c == '(')
+            .for_each(drop);
 
-        col_names.push(col_name);
+        // col names
+        let mut col_names = Vec::<String>::new();
+        while let Some(&cpeak) = it.peek() {
+            // skip spaces
+            if cpeak.is_whitespace() {
+                it.next();
+                continue;
+            }
+
+            // take until a new space
+            let col_name: String = it
+                .by_ref()
+                .take_while(|c| !c.is_whitespace())
+                // strip ticks or []
+                .filter(|c| !['\'', '"', '[', ']'].contains(&c))
+                .collect();
+
+            // skip until comma or end of statement
+            let _type: String = it.by_ref().take_while(|&c| c != ',').collect();
+
+            // println!("{:?} has type {:?}", col_name, _type);
+
+            col_names.push(col_name);
+        }
+
+        println!("col_names: {:?}", col_names);
+
+        // create table [{tablename}] ( [{col1}], [{col2}], ... )
+        let stmt = format!(
+            "create table [{table_name}] ( {} )",
+            col_names.iter().map(|c| format!("[{}]", c)).join(", ")
+        );
+        println!("{stmt:?}");
+
+        self.connection.execute(&stmt, ())?;
+
+        Ok(())
     }
 
-    println!("col_names: {:?}", col_names);
-}
+    fn copy_to_table<'a>(
+        &self,
+        fragment: &String,
+        lines: &mut impl Iterator<Item = &'a str>,
+    ) -> Result<()> {
+        let mut it = fragment
+            .strip_prefix("copy")
+            .expect(&format!("not copy?: {}", fragment))
+            .chars()
+            .peekable();
 
-fn copy_to_table<'a>(fragment: &String, lines: &mut impl Iterator<Item = &'a str>) {
-    // extract table name
-    let mut it = fragment
-        .strip_prefix("copy")
-        .expect(&format!("not copy?: {}", fragment))
-        .chars()
-        .peekable();
+        // extract table name
+        let table_name = it
+            .by_ref()
+            .take_while(|&c| c != '(')
+            .filter(|c| !['\'', '"', '[', ']'].contains(&c))
+            .collect::<String>();
+        let table_name = table_name.trim();
 
-    let table_name = it
-        .by_ref()
-        .take_while(|&c| c != '(')
-        .filter(|c| !['\'', '"', '[', ']'].contains(&c))
-        .collect::<String>();
-    let table_name = table_name.trim();
+        println!("table_name: {:?}", table_name);
 
-    println!("table_name: {:?}", table_name);
+        // we parse the cols from statement be aware of the col order
+        let col_names = it
+            .by_ref()
+            .take_while(|&c| c != ')')
+            .collect::<String>()
+            .split(",")
+            .map(|c| c.trim())
+            .map(|c| {
+                c.chars()
+                    .filter(|c| !['\'', '"', '[', ']'].contains(&c))
+                    .collect()
+            })
+            .collect::<Vec<String>>();
 
-    let ncols = it
-        .by_ref()
-        .take_while(|&c| c != ')')
-        .filter(|&c| c == ',')
-        .count()
-        + 1;
+        println!("ncols: {}", col_names.len());
 
-    println!("ncols: {}", ncols);
+        // insert into [{table_name}] ( [{col1}], [{col2}], ... ) values ( ?, ?, ... )
+        let stmt = format!(
+            "insert into [{table_name}] ( {} ) values ( {} )",
+            col_names.iter().map(|c| format!("[{}]", c)).join(", "),
+            std::iter::repeat("?").take(col_names.len()).join(", ")
+        );
+        println!("{stmt:?}");
 
-    let data = lines.take_while(|&l| l != r"\.");
+        let mut prepared_insert = self.connection.prepare(&stmt)?;
 
-    let data_rows = data
-        .map(|entry| {
+        let data_rows = lines.take_while(|&l| l != r"\.").map(|entry| {
             entry
                 .split("\t")
+                // TODO: map on dependant type
                 .map(|e| match e {
-                    r"\N" => "None",
-                    _ => e,
+                    r"\N" => None,
+                    // "f" => Some("false"),
+                    // "t" => Some("true"),
+                    _ => Some(e),
                 })
                 .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+        });
+        // .collect::<Vec<_>>();
 
-    println!("data rows: {:?}", data_rows.get(0));
+        // println!("data rows: {:?}", data_rows.get(0));
+
+        let mut rows_affected = 0;
+        for row in data_rows {
+            rows_affected += prepared_insert.execute(params_from_iter(row))?;
+        }
+        if rows_affected == 0 {
+            println!("no rows");
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
-    let dump_file = "dump.sql";
+    let import_op = ImportOp::new("dump.sql");
 
-    let content = fs::read_to_string(dump_file)?;
+    println!("start");
 
-    let mut lines = content.split("\n").peekable();
-    while let Some(&lpeak) = lines.by_ref().peek() {
-        if lpeak.starts_with("--") || lpeak.is_empty() {
-            lines.next();
-            continue;
-        }
-
-        let fragment = lines
-            .by_ref()
-            .take_while_inclusive(|l| !l.ends_with(";"))
-            .collect::<String>()
-            .to_lowercase();
-
-        if fragment.starts_with("create table") {
-            create_table(&fragment);
-        } else if fragment.starts_with("copy") {
-            copy_to_table(&fragment, lines.by_ref());
-            // break;
-        } else {
-            // println!("skipping {:?}", fragment);
-            continue;
-        }
-    }
+    import_op.parse()?;
 
     Ok(())
 }
